6cb54835d61be5fc740c1a52e451e53fa77eeae9
juliennuellas <julien.nuellas@gmail.com>
2018-02-15 15:38:14 +0100

    Modification fichier labyrinthe.py -> fonction read_labyrinth_from_json : faire des lignes une liste de string dans la liste labyrinth_structure

--- labyrinthe.py
+++ labyrinthe.py
@@ -2,23 +2,33 @@ import os
 import json
 
 def read_labyrinth_from_json(data_file, key):
-    """ This is the main function of the file """
-    lines_from_labyrinth = []
+    """ This function loads the labyrinth's structure into a list
+    containing each line. Each line is a list containing each stripe.
+    example : structure = [[stripe1_from_line1, stripe2_from_line1,...],
+     [stripe1_from_line2, stripe2_from_line2,...],...] """
+
+    labyrinth_structure = []
 
     directory = os.path.dirname(__file__) # We get the right path
     path_to_file = os.path.join(directory, 'sources', data_file) # We build the path
 
     with open(path_to_file, "r") as file:
         data = json.load(file)
-        for entry in data:
-            lines_from_labyrinth.append(entry[key])
+        for lines in data:
+            labyrinth_lines = []
+            for stripe in lines[key]:
+                labyrinth_lines.append(stripe)
+            labyrinth_structure.append(labyrinth_lines)
 
-    return lines_from_labyrinth
+    return labyrinth_structure
 
 def print_labyrinth(lines):
+    """ This function print the labyrinth into the terminal """
+
     for line in lines:
+        line = ''.join(line)
         print(line)
 
 if __name__ == "__main__":
-    lines = read_labyrinth_from_json('labyrinthe.json', 'line')
-    print_labyrinth(lines)
+    structure = read_labyrinth_from_json('labyrinthe.json', 'line')
+    print_labyrinth(structure)
