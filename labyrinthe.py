a0f4f23237704f50b74978ab3641d0f8f0e013a1
juliennuellas <julien.nuellas@gmail.com>
2018-02-15 14:33:45 +0100

    Modification labyrinthe.py : fonction ouvrir fichier labyrinthe

--- labyrinthe.py
+++ labyrinthe.py
@@ -1,7 +1,13 @@
 import os
 
-def main():
-    pass
+def print_labyrinthe(data_file):
+    """ This is the main function of the file """
+    directory = os.path.dirname(__file__) # We get the right path
+    path_to_file = os.path.join(directory, 'sources', data_file) # We build the path
 
-if __name__ = "__main__":
-    main()
+    with open(path_to_file, "r") as file:
+        for line in file:
+            print(line)
+
+if __name__ == "__main__":
+    print_labyrinthe('labyrinthe.txt')
