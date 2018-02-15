4c1e2b67d1986c9a58ffcf5a0d7f5fd69ca83ecc
juliennuellas <julien.nuellas@gmail.com>
2018-02-15 16:38:04 +0100

    Modification fichier labyrinthe.py : creation de la classe Level

--- labyrinthe.py
+++ labyrinthe.py
@@ -1,34 +1,46 @@
 import os
 import json
 
-def read_labyrinth_from_json(data_file, key):
-    """ This function loads the labyrinth's structure into a list
-    containing each line. Each line is a list containing each stripe.
-    example : structure = [[stripe1_from_line1, stripe2_from_line1,...],
-     [stripe1_from_line2, stripe2_from_line2,...],...] """
+class Level:
+    """ This class loads the structure of the labyrinth """
 
-    labyrinth_structure = []
+    def __init__(self, data_file, key):
+        self.data_file = data_file
+        self.key = key
+        self.structure = []
 
-    directory = os.path.dirname(__file__) # We get the right path
-    path_to_file = os.path.join(directory, 'sources', data_file) # We build the path
+    def generate_labyrinth_from_json(self):
+        """ This function loads the labyrinth's structure into a list
+        containing each line. Each line is a list containing each stripe.
+        example : structure = [[stripe1_from_line1, stripe2_from_line1,...],
+        [stripe1_from_line2, stripe2_from_line2,...],...] """
 
-    with open(path_to_file, "r") as file:
-        data = json.load(file)
-        for lines in data:
-            labyrinth_lines = []
-            for stripe in lines[key]:
-                labyrinth_lines.append(stripe)
-            labyrinth_structure.append(labyrinth_lines)
+        labyrinth_structure = []
 
-    return labyrinth_structure
+        directory = os.path.dirname(__file__) # We get the right path
+        path_to_file = os.path.join(directory, 'sources', self.data_file) # We build the path
 
-def print_labyrinth(lines):
-    """ This function print the labyrinth into the terminal """
+        with open(path_to_file, "r") as file:
+            data = json.load(file)
+            for lines in data:
+                labyrinth_lines = []
+                for stripe in lines[self.key]:
+                    labyrinth_lines.append(stripe)
+                labyrinth_structure.append(labyrinth_lines)
 
-    for line in lines:
-        line = ''.join(line)
-        print(line)
+        self.structure = labyrinth_structure
+
+    def print_labyrinth_into_terminal(self):
+        """ This function print the labyrinth into the terminal """
+
+        for line in self.structure:
+            line = ''.join(line)
+            print(line)
+
+def main():
+    labyrinth = Level('labyrinthe.json', 'line')
+    labyrinth.generate_labyrinth_from_json()
+    labyrinth.print_labyrinth_into_terminal()
 
 if __name__ == "__main__":
-    structure = read_labyrinth_from_json('labyrinthe.json', 'line')
-    print_labyrinth(structure)
+    main()
