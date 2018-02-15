4d6c247479e3b75c7a38cb4ad10d41ac1e6b52f3
juliennuellas <julien.nuellas@gmail.com>
2018-02-15 14:43:42 +0100

    Impression du labyrinthe dans la console à partir du json

--- labyrinthe.py
+++ labyrinthe.py
@@ -1,13 +1,24 @@
 import os
+import json
 
-def print_labyrinthe(data_file):
+def read_labyrinth_from_json(data_file, key):
     """ This is the main function of the file """
+    lines_from_labyrinth = []
+
     directory = os.path.dirname(__file__) # We get the right path
     path_to_file = os.path.join(directory, 'sources', data_file) # We build the path
 
     with open(path_to_file, "r") as file:
-        for line in file:
-            print(line)
+        data = json.load(file)
+        for entry in data:
+            lines_from_labyrinth.append(entry[key])
+
+    return lines_from_labyrinth
+
+def print_labyrinth(lines):
+    for line in lines:
+        print(line)
 
 if __name__ == "__main__":
-    print_labyrinthe('labyrinthe.txt')
+    lines = read_labyrinth_from_json('labyrinthe.json', 'line')
+    print_labyrinth(lines)
