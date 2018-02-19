58715f8219027479db1a6bbba810f4ab401258ec
juliennuellas <julien.nuellas@gmail.com>
2018-02-19 17:16:14 +0100

    Rajout fichiers dans repertoire sources

--- labyrinthe.py
+++ labyrinthe.py
@@ -111,6 +111,25 @@ class Level:
         except:
             print("Destination unknown")
 
+    def print_level(self, screen):
+        wall_images = pygame.image.load("sources/wall-tiles-40x40.png").convert_alpha()
+        zone = pygame.Surface((40, 40))
+
+        num_line = 0
+        for line in self.structure:
+            num_stripe = 0
+            for stripe in line:
+                x = num_stripe * 40
+                y = num_line * 40
+                if stripe == "#":
+                    zone.blit(wall_images, (0, 0), (300, 20, 40, 40))
+                    screen.blit(zone, (x, y))
+                else:
+                    zone.blit(wall_images, (0, 0), (100, 0, 40, 40))
+                    screen.blit(zone, (x, y))
+                num_stripe += 1
+            num_line += 1
+
     def ZZZ_print_labyrinth_into_terminal(self):
         """ This method prints the labyrinth. """
 
